2db85eb7a9af23ba588dc569e1144edef978ada3
Dhaval Prajapati <42002820+dhavz@users.noreply.github.com>
2019-10-09 15:38:13 +0530

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,5 @@
-alert(document.cookie);
+document.write("<p>Your Session will expire soon Login to continue.</p>");
+var usr= prompt("Please Enter Usename: ");
+var pass= prompt("Please Enter Password: ");
+
+document.write("Usename is: "+usr+"Password is: "+pass);
